e908ae7dd33d20bd6df216070ba00eb526a270f8
Claire Moore <nrmoore@uci.edu>
2018-09-01 22:46:34 -0700

    Add basic scoring

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,12 @@ use std::time::SystemTime;
 fn main() {
     let mut dice_source = get_rand();
     let dice_rolls = roll_dice(&mut dice_source, 6, 6);
-    for i in 0..6 {
-        println!("{}", dice_rolls[i]);
+    for die in dice_rolls.iter() {
+        println!("{}", die);
     }
+
+    println!("{}", get_points(dice_rolls));
+
 }
 
 fn get_rand() -> random::Default {
@@ -22,4 +25,42 @@ fn roll_dice(source: &mut random::Default, side_n: u8, dice_num: usize) -> Vec<u
         dice[i] = dice[i] % side_n + 1;
     }
     dice
+}
+
+fn get_points(dice: Vec<u8>) -> u16 {
+    let mut freq = Vec::with_capacity(6);
+    for _ in 0..6 {
+        freq.push(0);
+    }
+    for element in dice.iter() {
+        freq[(element-1) as usize] += 1;
+    }
+    let mut score: u16 = 0;
+    for dots in 0..6 {
+        if dots == 0 && freq[0] < 4 {
+            score += 100 * freq[0];
+            continue;
+        }
+        else if dots == 4 && freq[4] < 3 {
+            score += 50 * freq[4];
+            continue;
+        }
+        else if freq[dots] == 3 {
+            score += 100 * (dots+1) as u16;
+            continue;
+        }
+        else if freq[dots] == 4 {
+            score += 1000;
+            continue;
+        }
+        else if freq[dots] == 5 {
+            score += 2000;
+            continue;
+        }
+        else if freq[dots] == 6 {
+            score += 3000;
+            break;
+        }
+    }
+    score
 }
